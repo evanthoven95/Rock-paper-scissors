6d0134fe8762e09a32a9ad1f571d175f40c14636
evanthoven95 <evanthoven95@gmail.com>
2025-04-28 11:07:48 -0600

    Agrego condicionales que mostraran al ganador, segun la eleccion del jugador

--- tarea.py
+++ tarea.py
@@ -1,5 +1,5 @@
 import random
-
+import sys
 rock = '''
     _______
 ---'   ____)
@@ -33,9 +33,19 @@ computer=piedra_papel_tijeras[computer_random_number]
 answer_user=int(input('Elige un numero del 0 al 2: '))
 user=piedra_papel_tijeras[answer_user]
 print(f'Tu elegiste: {user} y la computadora eligio: {computer}')
+if answer_user==computer_random_number:
+    print('Empate')
 if answer_user == 2 and computer_random_number ==1:
-    print(scissors)
+    print(f'Ganaste, {scissors} contra {paper}')
+if answer_user==1 and computer_random_number==2:
+    print(f'Perdiste, {paper} contra {scissors}')
 if answer_user==1 and computer_random_number==0:
-    print(paper)
+    print(f'Ganaste, {paper} contra {rock}')
 if answer_user==0 and computer_random_number==1:
-    print(paper)
+    print(f'Perdiste, {rock} contra {paper}')
+if answer_user==2 and computer_random_number==0:
+    print(f'Perdiste, {scissors} contra {rock}')
+if answer_user==0 and computer_random_number==2:
+    print(f'Ganaste, {rock} contra {scissors}')
+if answer_user==3:
+    sys.exit()
